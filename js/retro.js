a8b60b412401309529e4e6b83bdf61f5ff88dd19
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-03-31 08:00:51 +0200

    Début du faisage d'ed/sam

--- js/retro.js
+++ js/retro.js
@@ -6,9 +6,28 @@ var bodyStyle =
 	 ,"color" : "orange"
 	};
 
+var buffer; /* Contenant le contenu, avant l'envoit */
+var cmd; /* La string de commande */
+
 function init() {
     /* Y'a mieux? */
     var convo = $(".convo")[0];
 
     $("body").css(bodyStyle);
+    $("#btngo").unbind("click");
+    $("#btngo").click(execcmd);
+    
+    $("<input type='text' autofocus id='cmd' />").insertAfter($("#btngo"));
+}
+
+function execcmd() {
+	cmd = $("#cmd").val();
+
+	switch(cmd[0]){
+	case "w":
+		envoieMessage();
+		break;
+	default:
+		console.log(cmd + " wat");
+	}
 }
