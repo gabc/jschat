88d9993864fa37c1a4c06a77d67b4dddc2d6800a
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-03-31 16:05:51 +0200

    Ajout de a,c,i,w

--- js/retro.js
+++ js/retro.js
@@ -2,32 +2,56 @@ $(init);
 
 var convo;
 var bodyStyle =
-	{"backgroundColor" : "black"
-	 ,"color" : "orange"
-	};
+	    {"backgroundColor" : "black"
+	     ,"color" : "orange"
+	    };
 
 var buffer; /* Contenant le contenu, avant l'envoit */
 var cmd; /* La string de commande */
+var input; /* Le truc contenant l'objet d'envoit */
 
 function init() {
-    /* Y'a mieux? */
-    var convo = $(".convo")[0];
-
-    $("body").css(bodyStyle);
-    $("#btngo").unbind("click");
-    $("#btngo").click(execcmd);
+	/* Y'a mieux? */
+	convo = $("#convo");
+	input = $("#inputtext");
+	
+	$("body").css(bodyStyle);
+	$("#btngo").unbind("click");
+	$("#btngo").click(execcmd);
     
-    $("<input type='text' autofocus id='cmd' />").insertAfter($("#btngo"));
+	$("<input type='text' autofocus id='cmd' />").insertAfter($("#btngo"));
 }
 
 function execcmd() {
 	cmd = $("#cmd").val();
+	var tmp;
+	var c;
 
-	switch(cmd[0]){
+	if (cmd.match(/^\//)) {
+		c = cmd.match(/\/(?:[a-zA-Z]*)\/([a-z])\/(?:.*)\//)[1];
+	} else {
+		c = cmd[0];
+	}
+	
+	switch(c){
+	case "a":
+		cmd = cmd.match(/\/(.*)\//)[1];
+		input.val(input.val() + cmd);
+		break;
+	case "c":
+		tmp = cmd.match(/\/(.*)\/c\/(.*)\//)[1];
+		tmp = new RegExp(tmp);
+		cmd = cmd.match(/\/(.*)\/c\/(.*)\//)[2];
+		input.val(input.val().replace(tmp, cmd));
+		break;
+	case "i":
+		cmd = cmd.match(/\/(.*)\//)[1];
+		input.val(cmd + input.val());
+		break;
 	case "w":
 		envoieMessage();
 		break;
 	default:
-		console.log(cmd + " wat");
+		console.log(c + " wat");
 	}
 }
