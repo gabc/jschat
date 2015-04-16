e88fc797d1e9f9ac83d9cb4bff4d6236416039d0
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-04-16 19:51:04 +0200

    Commit pre-refactor de la mort

--- js/three.js
+++ js/three.js
@@ -11,61 +11,72 @@ var renderer = new THREE.WebGLRenderer();
 var geometry = new THREE.PlaneGeometry( 5, 20, 32 );
 var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
 var plane = new THREE.Mesh( geometry, material );
+var controls = new THREE.FirstPersonControls(camera, renderer.domElement);
+
+var render = function () {
+	controls.update();
+	console.log("ouais");
+	renderer.render(scene, camera);
+	requestAnimationFrame( render );
+};
 
 function init () {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 	plane.rotation.x = Math.PI /2;
 	scene.add( plane );
+	controls.mouvementSpeed = 70;
+	controls.lookSpeed = 0.5;
+	controls.noFly = true;
+	controls.lookVertical = false;
 	camera.position.z = 5;
 	camera.position.y = 2;
 	render();
 }
 
-var render = function () {
-	requestAnimationFrame( render );
-	renderer.render(scene, camera);
-};
+// $(document).bind('keydown', function(e) {
+// 	if (e.key == 'a') {
+// 		var y = camera.rotation.y;
+// 		camera.position.z += Math.sin(y);
+// 		camera.position.x -= Math.cos(y);
+// 	}
+// 	if (e.key == 'd') {
+// 		var y = camera.rotation.y;
+// 		camera.position.z -= Math.sin(y);
+// 		camera.position.x += Math.cos(y);
+// 	}
+	
+// 	if (e.key == 's') {
+// 		var y = camera.rotation.y;
+// 		camera.position.z += Math.cos(y);
+// 		camera.position.x += Math.sin(y);
+// 	}
+// 	if (e.key == 'w') {
+// 		var y = camera.rotation.y;
+// 		camera.position.z -= Math.cos(y);
+// 		camera.position.x -= Math.sin(y);
+// 	}
+	
+	
+// 	if (e.key == '4') {
+// 		camera.rotation.y += 0.1;
+// 	}
+// 	if (e.key == '6') {
+// 		camera.rotation.y -= 0.1;
+// 	}
+	
+// 	if (e.key == '8') {
+// 		camera.rotation.x += 0.1; 
+// 	}
+// 	if (e.key == '2') {
+// 		camera.rotation.x -= 0.1;
+// 	}
 
-
-$(document).bind('keydown', function(e) {
-	if (e.key == 'a') {
-		var y = camera.rotation.y;
-		camera.position.z += Math.sin(y);
-		camera.position.x -= Math.cos(y);
-	}
-	if (e.key == 'd') {
-		var y = camera.rotation.y;
-		camera.position.z -= Math.sin(y);
-		camera.position.x += Math.cos(y);
-	}
-	
-	if (e.key == 's') {
-		var y = camera.rotation.y;
-		camera.position.z += Math.cos(y);
-		camera.position.x += Math.sin(y);
-	}
-	if (e.key == 'w') {
-		var y = camera.rotation.y;
-		camera.position.z -= Math.cos(y);
-		camera.position.x -= Math.sin(y);
-	}
-	
-	
-	if (e.key == '4') {
-		camera.rotation.y += 0.1;
-	}
-	if (e.key == '6') {
-		camera.rotation.y -= 0.1;
-	}
-	
-	if (e.key == '8') {
-		camera.rotation.x += 0.1; 
-	}
-	if (e.key == '2') {
-		camera.rotation.x -= 0.1;
-	}
-});    
+// 	if (e.key == 'f') {
+// 		newtext("Texte!");
+// 	}
+// })
+;    
 
 function newtext (txt) {
 	var geometry = new THREE.TextGeometry(txt, { face: "helvetiker", size: 1.0, height: 0.1} );
