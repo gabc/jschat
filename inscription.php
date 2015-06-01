9460a84e9aa1e3c891c47b30fc1371ae87156022
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-06-01 16:54:42 -0400

    Inscription mieux

--- inscription.php
+++ inscription.php
@@ -10,15 +10,16 @@
 	<head>
 		<meta charset="UTF-8"> 
 		<script src="js/jquery.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/inscription.css">
 	</head>
 	<body>
-	  <form action="inscription.php" method="post">
-	    Matricule: <input type="text" autofocus name="mat"/>
-	    Prenom: <input type="text" autofocus name="prenom"/>
-	    Nom: <input type="text" autofocus name="nom"/>	    
-	    Pseudo: <input type="text" autofocus name="usr"/>
-	    Phrase: <input type="text" name="phrase" />
-	    Mot de passe: <input type="password" name="pwd" />
+	  <form action="inscription.php" method="post" id="frm">
+	    Matricule: <input type="text" autofocus name="mat" class="in"/>
+	    Prenom: <input type="text" name="prenom" class="in"/>
+	    Nom: <input type="text" name="nom" class="in"/>
+	    Pseudo: <input type="text" name="usr" class="in"/>
+	    Phrase: <input type="text" name="phrase" class="in"/>
+	    Mot de passe: <input type="password" name="pwd" class="in"/>
 	    <button id="ntnlogin">go.</button>
 	  </form>
 	</body>
